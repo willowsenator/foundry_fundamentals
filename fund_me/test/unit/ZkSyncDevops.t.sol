1b02ab18d970733a1febd4f2c9f3e8a6e422a6f7
willownsenator <willownsenator@gmail.com>
2024-08-04 01:30:27 +0200

    modules

---
+++ fund_me/test/unit/ZkSyncDevops.t.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ZkSyncChainChecker} from "lib/fondry-devops/src/ZkSyncChainChecker.sol";
+import {FounFoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.sol";
